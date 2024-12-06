8487d42460f76016cf9ef2fb823f5e948d63b7a7
kelvinbmob004 <kelvinb@mobiquityinc.com>
2024-12-06 20:34:11 +0530

    Added Test to check if we are getting the sum of numbers if there are more than one numbers

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -8,4 +8,8 @@ describe("StringCalculator --> add", () => {
   it("should return the number itself if the string has one number", () => {
     expect(StringCalculator.add("7")).toBe(7);
   });
+
+  it("should return the sum of the numbers if the string has more than one number", () => {
+    expect(StringCalculator.add("7,9")).toBe(16);
+  });
 });
