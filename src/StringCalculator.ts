b4edc00c7aa6b8418e7403b4d27e594be98b5da6
kelvinbmob004 <kelvinb@mobiquityinc.com>
2024-12-06 20:36:44 +0530

    Added the code to do the sum of all the numbers passed with comma seperated

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,6 +3,14 @@ export class StringCalculator {
     if (numbers === "") {
       return 0;
     }
-    return parseInt(numbers);
+
+    // Grab all the numbers in array to do the summation
+    const numbersArray = numbers.split(",");
+    let sumOfNumbers = 0;
+
+    // Loop through all the numbers and add into variable
+    numbersArray.forEach((value) => (sumOfNumbers += parseInt(value)));
+
+    return sumOfNumbers;
   }
 }
