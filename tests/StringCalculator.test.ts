e4a32f25c1fbb6ded3ece9abe48717cff7446e0c
kelvinbmob004 <kelvinb@mobiquityinc.com>
2024-12-06 20:27:24 +0530

    Added new test to check if the function returns the passed number if only one number is passed

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -1,7 +1,11 @@
 import { StringCalculator } from "../src/StringCalculator";
 
-describe("StringCalculator Operation", () => {
+describe("StringCalculator --> add", () => {
   it("should return 0 for the empty string passed in add function", () => {
     expect(StringCalculator.add("")).toBe(0);
+  });
+
+  it("should return the number itself if the string has one number", () => {
+    expect(StringCalculator.add("7")).toBe(7);
   });
 });
