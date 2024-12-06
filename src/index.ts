3c35d48a15dba2e4c433158a16d346bfc5221046
kelvinbmob004 <kelvinb@mobiquityinc.com>
2024-12-06 21:23:40 +0530

    Removed the unnecessary logging

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { StringCalculator } from "./StringCalculator";
 
 const numbersToAdd = process.argv[2];
 
-console.log(numbersToAdd);
-
 const sum = StringCalculator.add(numbersToAdd);
 
 console.log(sum);
